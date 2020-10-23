2bfb8c9d7d883c8d9c7712f647932670b9079f9e
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-10-23 19:09:49 -0300

    [Web] - actions and reducers

---
+++ src/components/CatalogItem.tsx
@@ -0,0 +1,33 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addProductToCart } from '../store/modules/cart/action';
+import { IProduct } from '../store/modules/cart/types';
+
+interface CatalogItemProps {
+  product: IProduct;
+}
+
+const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleAddProductToCart = useCallback(() => {
+    dispatch(addProductToCart(product))
+  }, [dispatch, product]);
+
+  return (
+    <article>
+      <strong>{product.title}</strong>{" - "}
+      <span>{product.price}</span>{"  "}
+
+      <button 
+        type="button" 
+        onClick={handleAddProductToCart}
+      >
+        Comprar
+      </button>
+    </article>
+  )
+}
+
+export default CatalogItem;
