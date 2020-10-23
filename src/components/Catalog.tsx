bda14b596b0ff0faa8493784d8db85ade3d3bef8
Wesley Feitosa <jwesleydasilva@gmail.com>
2020-10-23 19:11:32 -0300

    [Web] - test

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -5,6 +5,7 @@ import CatalogItem from './CatalogItem';
 
 const Catalog: React.FC = () => {
   const [catalog, setCatalog] = useState<IProduct[]>([]);
+  
 
   useEffect(() => {
     api.get('/products').then(response => {
